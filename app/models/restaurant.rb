4fb8ee587d38c4b98b62c0d4317834cf29b17a0a
Roger Tosbotn <rogertosb@gmail.com>
2020-02-19 13:42:02 +0100

    both models are fully functioning

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
-
   has_many :reviews, dependent: :destroy
+
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
