c8482180fdf50ddb4f2faa4d5824e302709dea82
Roger Tosbotn <rogertosb@gmail.com>
2020-02-19 15:48:37 +0100

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
+  # get 'reviews/new'
+  # get 'reviews/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants do
     resources :reviews, only: [:new, :create]
